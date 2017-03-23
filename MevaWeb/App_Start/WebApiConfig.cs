8685b51037debe38abb29756350e1f3bbc1d9f8f
DESKTOP\Sarunas <visarunas@gmail.com>
2017-03-23 20:13:31 +0200

    First Blood

--- MevaWeb/App_Start/WebApiConfig.cs
+++ MevaWeb/App_Start/WebApiConfig.cs
@@ -15,9 +15,9 @@ namespace MevaWeb
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+              name: "DefaultApi",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional }
             );
         }
     }
---
+++ MevaWeb/DBA/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Data.Entity;
+using MevaWeb.Models;
+
+namespace MevaWeb.DBA
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext() : base("Connection")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
